ca894238d3886c0676a1ffd92057512d73d57993
lisse.dyck <lisse.dyck@leerling.piustien.net>
2019-11-19 05:43:57 -0500

    alles

---
+++ code/Domain/Logboek.php
@@ -0,0 +1,13 @@
+<?php
+class Logboek{
+  private $_naam = "";
+  private $_datum = Datum();
+  private $_tijd = Tijd();
+  private $_categorie = Categorie();
+  
+  
+  function __construct($naam, $datum, $tijd, $categorie){
+    
+  }
+}
+?>
