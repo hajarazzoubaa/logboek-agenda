dfa29aa1b65d89ab582d5b090e166304bcf52cd7
lisse.dyck <lisse.dyck@leerling.piustien.net>
2020-02-02 07:36:24 -0500

    alles

---
+++ code/Domain/Datum/Tijd.php
@@ -0,0 +1,18 @@
+<?php
+class Tijd{
+  private $_uur: 0;
+  private $_minuten: 0;
+  
+  function __construct($uur, $minuten){
+    $this ->_uur = $uur;
+    $this ->_minuten = $minuten;
+  }
+  
+  if($uur > 24){
+    $uur = 24;
+  }
+  if($minuten > 59){
+    $minuten = 59;
+  }
+}
+?>
