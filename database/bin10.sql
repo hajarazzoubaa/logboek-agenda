d867356e84586c36196b66f41ad73b8f61375a54
hajar.azzoubaa <hajar.azzoubaa@leerling.piustien.net>
2020-03-22 14:32:14 -0400

    alles

--- database/bin10.sql
+++ database/bin10.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: www.piustien.net
--- Gegenereerd op: 22 mrt 2020 om 16:11
+-- Gegenereerd op: 22 mrt 2020 om 17:46
 -- Serverversie: 5.5.62-0+deb8u1
 -- PHP-versie: 7.2.19
 
@@ -33,6 +33,14 @@ CREATE TABLE `Administrator` (
   `wachtwoord` varchar(30) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Gegevens worden geëxporteerd voor tabel `Administrator`
+--
+
+INSERT INTO `Administrator` (`email`, `wachtwoord`) VALUES
+('hajar.azzoubaa@leerling.piusti', 'leerling01'),
+('loulou-11@gmail.com', 'loulou123');
+
 -- --------------------------------------------------------
 
 --
@@ -45,6 +53,14 @@ CREATE TABLE `Afspraak` (
   `tijdstip` time NOT NULL,
   `persoon` varchar(30) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Gegevens worden geëxporteerd voor tabel `Afspraak`
+--
+
+INSERT INTO `Afspraak` (`naam`, `locatie`, `tijdstip`, `persoon`) VALUES
+('Brunch', 'Kielpark', '12:12:00', 'Lofifi'),
+('tandarts', 'Abdijstraat 15', '18:12:12', 'Dr Simons');
 
 -- --------------------------------------------------------
 
@@ -69,6 +85,14 @@ CREATE TABLE `Evenement` (
   `tijd` time NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Gegevens worden geëxporteerd voor tabel `Evenement`
+--
+
+INSERT INTO `Evenement` (`naam`, `locatie`, `tijd`) VALUES
+('Musical Justin Bieber', 'stadsschouwburg', '20:10:00'),
+('Verjaardagsfeest', 'Abdijstraat 15', '18:10:00');
+
 -- --------------------------------------------------------
 
 --
@@ -79,6 +103,14 @@ CREATE TABLE `Feestdag` (
   `naam` varchar(30) NOT NULL,
   `datum` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Gegevens worden geëxporteerd voor tabel `Feestdag`
+--
+
+INSERT INTO `Feestdag` (`naam`, `datum`) VALUES
+('Verjaardag Hajar', '2020-04-24'),
+('Verjaardag Lisse', '2020-07-18');
 
 -- --------------------------------------------------------
 
@@ -92,6 +124,14 @@ CREATE TABLE `Gebruiker` (
   `wachtwoord` tinytext NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Gegevens worden geëxporteerd voor tabel `Gebruiker`
+--
+
+INSERT INTO `Gebruiker` (`email`, `naam`, `wachtwoord`) VALUES
+('gebruiker01@gmail.com', 'gebruiker01', 'gebruiker01'),
+('gebruiker02@gmail.com', 'gebruiker02', 'gebruiker02');
+
 -- --------------------------------------------------------
 
 --
@@ -102,6 +142,14 @@ CREATE TABLE `Herinnering` (
   `naam` varchar(30) NOT NULL,
   `tijd` time NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Gegevens worden geëxporteerd voor tabel `Herinnering`
+--
+
+INSERT INTO `Herinnering` (`naam`, `tijd`) VALUES
+('Bloemen kopen voor Valentijn', '15:00:00'),
+('Kinderen ophalen', '14:20:00');
 
 -- --------------------------------------------------------
 
@@ -115,6 +163,14 @@ CREATE TABLE `Persoon` (
   `wachtwoord` tinytext NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Gegevens worden geëxporteerd voor tabel `Persoon`
+--
+
+INSERT INTO `Persoon` (`email`, `naam`, `wachtwoord`) VALUES
+('gebruiker01@gmail.com', 'Gebruiker01', 'gebruiker01'),
+('gebruiker02@gmail.com', 'gebruiker02', 'gebruiker02');
+
 -- --------------------------------------------------------
 
 --
@@ -124,6 +180,14 @@ CREATE TABLE `Persoon` (
 CREATE TABLE `Taak` (
   `naam` varchar(30) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Gegevens worden geëxporteerd voor tabel `Taak`
+--
+
+INSERT INTO `Taak` (`naam`) VALUES
+('Taak Frans'),
+('Taak wiskunde');
 
 --
 -- Indexen voor geëxporteerde tabellen
