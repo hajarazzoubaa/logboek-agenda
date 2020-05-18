575dae05a977ec3eedc5787f045dc2267223bdaf
lisse.dyck <lisse.dyck@leerling.piustien.net>
2020-05-18 08:03:40 -0400

    alles

---
+++ data/DatabaseConnection.php
@@ -0,0 +1,32 @@
+<?php
+class DatabaseConnection {
+  private $_host = "";
+  private $_username = "";
+  private $_password = "";
+  private $_database = "";
+  
+  private $_connection = null;
+  
+  function __construct(){
+    $this->_host = "www.piustien.net";
+    $this-> _username= "";
+    $this-> _password = "";
+    $this-> _database = "";
+    
+    $this ->_connection = new mysqli($this->_host, $this-> _username, $this-> _password, $this-> _databse);
+    
+    if($this-> _connection == false){
+      die("connection failed" . myspli_connect_error());
+      
+    }
+    function QueryData()
+    //SQl query die alle slideshows opvraagt
+      
+      
+    //SQL query die alle slides per slideshow opvraagt
+    // SQl query die alle items per slide opvraagt
+    
+  }
+}
+
+?>
